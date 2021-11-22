36dd16c4ff8ff1a5d6abd3885fc732b1c76d1a55
MatheusCurticeiro <87614400+MatheusCurticeiro@users.noreply.github.com>
2021-11-22 16:29:46 -0300

    Add files via upload

---
+++ Xylophone2/Xylophone2/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  Xylophone2
+//
+//  Created by Matheus Curticeiro on 26/08/21.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func keyPressed(_ sender: UIButton) {
+        
+      
+        playSound(soundName: sender.currentTitle!)
+        
+        sender.alpha = 0.5
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2){
+            sender.alpha = 1.0
+        }
+        
+    }
+    
+    func playSound(soundName: String) {
+            let url = Bundle.main.url(forResource: soundName, withExtension: "wav")
+            player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+                    
+        }
+}
+
